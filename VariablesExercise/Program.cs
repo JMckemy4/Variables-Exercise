eb91ac6670e7e8806c6553efb1f31187b1b9afe0
jared mckemy <jaredmkcemy4@gmail.com>
2023-08-24 12:55:13 -0700

    went through variable steps

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -3,8 +3,17 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+    { 
+        string dogName = "Daisy";
+        int dogAge = 8;
+        char testLetter = 'D';
+        bool isTired = false;
+        decimal price = 21;
+        double love = 21.99;
+
+        Console.WriteLine($"{dogName} and {dogAge} are relavant to when we got her. {testLetter}. + She is mean and upset{isTired}");
+        Console.WriteLine();
+
         }
     }
 }
